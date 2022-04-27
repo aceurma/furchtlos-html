4a0196619d6802acc01e8c208eae54c307460f19
kcbolisay <karlo@seirim.com>
2022-04-27 13:33:53 +0800

    Slide updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,9 @@
 $(document).ready(function(){
+    AOS.init({
+        duration: 1000, // values from 0 to 3000, with step 50ms
+        easing: 'ease', // default easing for AOS animations
+        once: true, // whether animation should happen only once - while scrolling down
+      });
 
     /* Dropdown Fade Effect */
 	var fadeIn = "animate__fadeIn";
